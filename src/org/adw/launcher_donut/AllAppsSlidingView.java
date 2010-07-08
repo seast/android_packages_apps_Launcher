9b2a62ce6374293ec039ea267b3d0c26c0533bd8
AnderWeb <anderweb@gmail.com>
2010-07-08 15:25:21 +0200

    Second part of the themes support. Warning, may need refactoring.
    Added the preferences ui to pick and apply themes, and added themable support
    for a lot of things.
    I'm completely sure i could refactor the code so it can be all managed inside
    a custom class instead all over the other files...

--- src/org/adw/launcher_donut/AllAppsSlidingView.java
+++ src/org/adw/launcher_donut/AllAppsSlidingView.java
@@ -232,7 +232,7 @@ public class AllAppsSlidingView extends AdapterView<ApplicationsAdapter> impleme
     
     void setLauncher(Launcher launcher) {
         mLauncher = launcher;
-        setSelector(IconHighlights.getDrawable(mLauncher,false));
+        setSelector(IconHighlights.getDrawable(mLauncher,IconHighlights.TYPE_DESKTOP));
     }
 	@Override
 	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
@@ -1016,7 +1016,6 @@ public class AllAppsSlidingView extends AdapterView<ApplicationsAdapter> impleme
         } else {
             child = mAdapter.getView(position, null, this);
         }
-        child.setBackgroundDrawable(IconHighlights.getDrawable(mLauncher, false));
         return child;
     }
     public int getPageCount(){
